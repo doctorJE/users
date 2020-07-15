02aa4adeb1d6fc7de3af7a424e9483e7f39e5a03
Robert <jerry_chen@mail.chungyo.net>
2020-07-15 20:32:21 +0800

    建立 user 資料表

---
+++ database/migrations/2020_07_15_122537_create_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->id()
+                ->comment('編號');
+            $table->string('account', 50)
+                ->unique('unique_account')
+                ->comment('帳號');
+            $table->string('password', 60)
+                ->comment('密碼');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
