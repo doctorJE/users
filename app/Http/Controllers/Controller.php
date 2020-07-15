1fd20616ac0f46dff891a405ea9a7a1534569f51
Robert <jerry_chen@mail.chungyo.net>
2020-07-15 22:48:10 +0800

    加入 Controller 參數檢查機制

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Classes\Converter\OutputConverter;
+use App\Classes\Error\ApiError;
+use App\Classes\Exceptions\InvalidInputValueException;
+use App\Classes\ResponseCreator;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Execute an action on the controller.
+     *
+     * @param string $method
+     * @param array $parameters
+     * @return mixed
+     */
+    public function callAction($method, $parameters)
+    {
+        try {
+            return parent::callAction($method, $parameters);
+        } catch (InvalidInputValueException $exception) {
+            return ResponseCreator::createResponse(
+                OutputConverter::convertResult(false, new ApiError($exception->getCode()))
+                , Response::HTTP_BAD_REQUEST
+            );
+        }
+    }
 }
